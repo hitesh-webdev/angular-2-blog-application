0ffc6534c772c2ac407ecd5c18f320bd17437ec8
Hitesh Aswani <hitesh.aswani@infostretch.com>
2017-08-09 17:27:15 +0530

    Configured all routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
@@ -9,6 +10,22 @@ import { PostComponent } from './post/post.component';
 import { SidebarComponent } from './post/sidebar/sidebar.component';
 import { FooterComponent } from './footer.component';
 import { PostDetailComponent } from './post/post-detail/post-detail.component';
+import { LoginComponent } from './login/login.component';
+import { AddPostComponent } from './post/add-post/add-post.component';
+import { NotFoundComponent } from './not-found.component';
+
+/* Routing paths
+============================================ */
+
+const appRoutes: Routes = [
+  {path: '', component: PostComponent, children: [
+    {path: '', component: PostListComponent},
+    {path: 'post-detail/:id', component: PostDetailComponent}
+  ]},
+  {path: 'login', component: LoginComponent},
+  {path: 'add-post', component: AddPostComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
@@ -19,10 +36,14 @@ import { PostDetailComponent } from './post/post-detail/post-detail.component';
     PostComponent,
     SidebarComponent,
     FooterComponent,
-    PostDetailComponent
+    PostDetailComponent,
+    LoginComponent,
+    AddPostComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,5 +1,5 @@
 import { Comment } from './comment';
 
 export class Post {
-    constructor(public title: string, public author: string, public timestamp: string, public imagePath: string, public content: string, public tags: string[], public comments: Comment[]) {}
+    constructor(public postId: number, public title: string, public author: string, public timestamp: string, public imagePath: string, public content: string, public tags: string[], public comments: Comment[]) {}
 }
